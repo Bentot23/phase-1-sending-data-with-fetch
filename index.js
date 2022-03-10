121276952295cfab69e757948a8b9a7271a0ff43
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-03-10 00:37:18 -0600

    commit tree f718548ed81966d9152f04ed7115767d291d2ba6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({"name": userName, "email": userEmail})
+    })    
+    .then(resp => resp.json())
+    .then(data => {
+        const newId = data.id;
+        const p = document.createElement('p');
+        p.textContent = newId;
+        const body = document.querySelector('body');
+        body.append(p)
+    })
+    .catch(someError => {
+        const p = document.createElement('p');
+        p.textContent = someError.message;
+        const body = document.querySelector('body');
+        body.append(p)
+    })
+}
